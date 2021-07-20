9a4939bc65fd1f06ca4e8671debabbef3a5c5ef6
lephongplus2016 <lephongplus2016@gmail.com>
2021-07-20 10:45:12 +0700

    add a server nodejs

---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let app = express();
+app.listen(3000);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => res.render("home"));
+console.log("Server listen at port 3000");
